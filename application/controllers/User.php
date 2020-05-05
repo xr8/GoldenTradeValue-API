bd4ee28114742c0b3398dd1b53455ccf1f5f3f5c
Jorge Garibaldo <biohizard@gmail.com>
2020-05-05 03:01:06 -0500

    Email
    demo online

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -5,6 +5,19 @@ class User extends CI_Controller {
     //--->
     public function __construct(){
         parent::__construct();
+        //EMAIL
+        $configEmail = Array(
+            'useragent' => 'Tech LUXZA SYSTEMS',
+            'protocol'  => 'smtp',
+            'smtp_host' => 'mail.gtvsa.com',
+            'smtp_port' =>  587,
+            'smtp_user' => 'passwordperdido@gtvsa.com',
+            'smtp_pass' => '12345aeiou',
+            'mailtype'  => 'html',
+            'charset'   => 'utf-8'
+            );
+            $this->load->library('email', $configEmail);
+
         // Your own constructor code
         $this->load->database();
         $this->default= $this->load->database('default', TRUE);
@@ -12,14 +25,14 @@ class User extends CI_Controller {
             $this->load->model('log/Model_log');
             $this->load->model('user/Querys');
             $xr8_data = $this->Model_log->logNew();
-        }
+    }
     //--->
 
     //--->
     public function index(){
         $xr8_data = $this->Model_log->logNew();
         $this->output->set_content_type('application/json')->set_output(json_encode($xr8_data));
-        }
+    }
     //--->
 
         //--->
@@ -54,8 +67,7 @@ class User extends CI_Controller {
                         $xr8_data   = $this->Querys->usuarioNew();
                         $this->output->set_content_type('application/json')->set_output(json_encode($xr8_data));
                     }
-
-            }
+        }
         //--->
 
         //--->
@@ -80,29 +92,58 @@ class User extends CI_Controller {
 
                 $xr8_data   = $this->Querys->userRead($id_advance,$all);
                 $this->output->set_content_type('application/json')->set_output(json_encode($xr8_data));
-
-            }
+        }
         //--->
 
         //--->
         public function updatedata(){
-
             $id_advance = $_POST['id_advance'];
             $xr8_data = $this->Querys->socios_actualizar($id_advance);
-
             //----->json
             $this->output->set_content_type('application/json')->set_output(json_encode($xr8_data));
-
-            }
+        }
         //--->
 
         //--->
         public function deletedata(){
-            $xr8_data = $this->Querys->socios_borrar();
+          $xr8_data = $this->Querys->socios_borrar();
             //----->json
             $this->output->set_content_type('application/json')->set_output(json_encode($xr8_data));
-            }
+        }
         //--->
 
+        //--->
+        public function lostpassword(){
+          //email
+          $LSf47vWou0wNVEsEuT1i = $_POST['LSf47vWou0wNVEsEuT1i'];
+
+          $data['demo'] = 'Demo de Email';
+
+          $list = array('biohizard@gmail.com');
+          $bcc  = array('info@gtvsa.com',);
+
+          $this->email->set_newline("\r\n");
+          $this->email->from('info@gtvsa.com','GTV / Recuperacion');
+          $this->email->to($list);
+          //$this->email->bcc($bcc);
+          $this->email->subject('GTV Recuperacion');
+
+              $body = $this->load->view('user/lostpassword', $data, TRUE);
+              $this->email->message($body);
+
+              try{
+
+                  $this->email->send();
+                  $xr8_data['sendemail']="Email Send";
+
+                  }catch(Exception $e){
+
+                      $e->getMessage();
+                      $xr8_data['sendemail']="Email No Send";
+
+                      }
+                      $this->output->set_content_type('application/json')->set_output(json_encode($xr8_data));
+        }
+        //--->
 //----->
 }
