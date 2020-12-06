facec9e4652e14035b8fe438b886f92d5f969ef2
Francisco Garibaldo GK <biohizard@hotmail.com>
2020-12-05 23:05:12 -0600

    GTV Update
    Api time

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -17,8 +17,6 @@ class Api extends CI_Controller {
     public function index(){
       $xr8_data['Api Connection'] = random_string('alpha', 20);
       $xr8_data['Api Time']       = date("Y-m-d H:i:s.u");
-
-      console.log(new Date(dt.now()));
         $this->output->set_content_type('application/json')->set_output(json_encode($xr8_data));
         }
     //--->
